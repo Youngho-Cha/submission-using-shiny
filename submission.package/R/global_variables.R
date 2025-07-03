efbc92c632ff7863b060b57333382ba23308b24c
Youngho-Cha <yh.cha@jnpmedi.com>
2025-07-03 14:48:46 +0900

    Delete submission.package directory

--- submission.package/R/global_variables.R
+++
@@ -1,2 +0,0 @@
-
-utils::globalVariables(c("Estimate", "Lower_95_CI", "Upper_95_CI"))
--- submission.package/docker/start_app.R
+++
@@ -1,6 +0,0 @@
-shiny::runApp(
-  appDir="/home/app/shiny",
-  host="0.0.0.0",
-  port=3838,
-  launch.browser=FALSE
-)
